cbec27d4c68371d8f8bca15268cb0ad1ead3885c
gabrielvalle00 <gabrivalle8@gmailcom>
2025-06-29 19:10:32 -0300

    fix: corrige tipagem do onError do vídeo em ProjectCard para TypeScript

--- Portifolio/components/sub/ProjectCard.tsx
+++ Portifolio/components/sub/ProjectCard.tsx
@@ -55,9 +55,9 @@ const ProjectCard = ({
             autoPlay
             playsInline
             onError={(e) => {
-              const parent = e.target.parentNode;
+              const parent = (e.target as HTMLElement).parentNode;
               if (parent) {
-                parent.innerHTML = `<img src='${src}' alt='${title}' style='width:100%;height:100%;object-fit:cover;border-radius:inherit;' />`;
+                (parent as HTMLElement).innerHTML = `<img src='${src}' alt='${title}' style='width:100%;height:100%;object-fit:cover;border-radius:inherit;' />`;
               }
             }}
           />
